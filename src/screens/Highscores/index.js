e2712b936a4ec893ca4c775bad27601da7dbb1ff
karimadnan <karimadnan@>
2021-05-30 20:52:55 +0200

    highscores edit

--- src/screens/Highscores/index.js
+++ src/screens/Highscores/index.js
@@ -84,104 +84,110 @@ const Highscores = ({ highscoresList, mobile }) => {
         <div className="content-border">
           <div className="row">
             {/* MAIN CONTENT START */}
-            <div className="col-lg-12 col-md-12 col-sm-12 panel panel-default mt-3">
-              <h4 className="font-weight-bold p-2">Highscores Filter</h4>
-              <div className="col-md-12 col-sm-12 panel highscores-filter">
-                {vocations.map((voc, index) => (
-                  <div className={`col-lg-2 col-md-3 col-sm-4 ${filterVocation === voc.filter ? 'highscore-filter-box' : 'highscore-filter-text'}`} key={index} onClick={() => onValueChangeVocation(voc.filter)}>{voc.name}</div>
-                ))}
-              </div>
-              <div className="col-md-12 col-sm-12 panel highscores-filter">
-                {skills.map((skill, index) => (
-                  <div className={`col-lg-2 col-md-3 col-sm-4 ${filterSkill === skill.filter ? 'highscore-filter-box' : 'highscore-filter-text'}`} key={index} onClick={() => onValueChangeSkill(skill.filter)}>
-                    {skill.name}
+            <div className="container">
+              <div className="col-lg-12 col-md-12 col-sm-12 highscore-panel">
+                <div className="row">
+                  <div className="col-lg-12 col-md-12 col-sm-12 font-weight-bold p-2 highscore-header">Highscores Filter</div>
+                  <div className="col-md-12 col-sm-12 panel highscores-filter">
+                    {vocations.map((voc, index) => (
+                      <div className={`col-lg-2 col-md-3 col-sm-4 ${filterVocation === voc.filter ? 'highscore-filter-box' : 'highscore-filter-text'}`} key={index} onClick={() => onValueChangeVocation(voc.filter)}>{voc.name}</div>
+                    ))}
                   </div>
-                ))}
+                  <div className="col-md-12 col-sm-12 panel highscores-filter">
+                    {skills.map((skill, index) => (
+                      <div className={`col-lg-2 col-md-3 col-sm-4 ${filterSkill === skill.filter ? 'highscore-filter-box' : 'highscore-filter-text'}`} key={index} onClick={() => onValueChangeSkill(skill.filter)}>
+                        {skill.name}
+                      </div>
+                    ))}
+                  </div>
+                </div>
               </div>
             </div>
-            <div className="col-lg-12 col-md-12 col-sm-12 panel panel-default mt-3">
-              <h4 className="font-weight-bold p-2">Highscores</h4>
-              <div className="row">
-                <table className="table table-striped table-highscores">
-                  <thead>
-                    <tr>
-                      <th width="1%">Rank</th>
-                      <th width="6%">Name</th>
-                      <th width="5%">{skillsName}</th>
-                      <th width="8%">Vocation</th>
-                      {filterSkill === 'level' && <th width="10%">Experience Points</th>}
-                    </tr>
+            <div className="container">
+              <div className="col-lg-12 col-md-12 col-sm-12 highscore-panel">
+                <div className="row">
+                  <div className="col-lg-12 col-md-12 col-sm-12 font-weight-bold p-2 highscore-header">Highscores</div>
+                  <table className="table table-striped table-highscores">
+                    <thead>
+                      <tr>
+                        <th width="1%">Rank</th>
+                        <th width="6%">Name</th>
+                        <th width="5%">{skillsName}</th>
+                        <th width="8%">Vocation</th>
+                        {filterSkill === 'level' && <th width="10%">Experience Points</th>}
+                      </tr>
 
-                    {playerList.map((props, index) => {
-                      const skill =
-                        filterSkill === 'level'
-                          ? props.level
-                          : props[filterSkill];
-                      return (
-                        <tr key={props.id} className={index % 2 !== 0 ? 'highscores-tr-odd' : ''}>
-                          <td>{pageInitial * characterPerPage + index + 1}</td>
+                      {playerList.map((props, index) => {
+                        const skill =
+                          filterSkill === 'level'
+                            ? props.level
+                            : props[filterSkill];
+                        return (
+                          <tr key={props.id} className={index % 2 !== 0 ? 'highscores-tr-odd' : ''}>
+                            <td>{pageInitial * characterPerPage + index + 1}</td>
 
-                          <td style={{ fontWeight: 'bold' }}>
-                            <Link to={`/character/${props.name}`}>
-                              {props.name}
-                            </Link>
-                          </td>
-                          <td>{skill}</td>
-                          <td>{characterVocations[props.vocation]}</td>
-                          {filterSkill === 'level' && <td>{props.experience.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</td>}
-                        </tr>
-                      );
-                    })}
-                  </thead>
-                </table>
-              </div>
-              <div className="row justify-content-center">
-                <ul className="pagination my-4">
-                  {pageInitial <= 0 ? (
-                    <li className="page-item">
-                      <button
-                        className="page-link disabled mr-3"
-                        aria-label="Previous"
-                        onClick={(e) => setPageInitial(pageInitial - 1)}
-                        disabled
-                      >
-                        &#8249;
+                            <td style={{ fontWeight: 'bold' }}>
+                              <Link to={`/character/${props.name}`}>
+                                {props.name}
+                              </Link>
+                            </td>
+                            <td>{skill}</td>
+                            <td>{characterVocations[props.vocation]}</td>
+                            {filterSkill === 'level' && <td>{props.experience.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</td>}
+                          </tr>
+                        );
+                      })}
+                    </thead>
+                  </table>
+                </div>
+                <div className="row justify-content-center">
+                  <ul className="pagination my-4">
+                    {pageInitial <= 0 ? (
+                      <li className="page-item">
+                        <button
+                          className="page-link disabled mr-3"
+                          aria-label="Previous"
+                          onClick={(e) => setPageInitial(pageInitial - 1)}
+                          disabled
+                        >
+                          &#8249;
                       </button>
-                    </li>
-                  ) : (
-                    <li className="page-item">
-                      <button
-                        className="page-link round mr-3"
-                        aria-label="Previous"
-                        onClick={(e) => setPageInitial(pageInitial - 1)}
-                      >
-                        &#8249;
+                      </li>
+                    ) : (
+                      <li className="page-item">
+                        <button
+                          className="page-link round mr-3"
+                          aria-label="Previous"
+                          onClick={(e) => setPageInitial(pageInitial - 1)}
+                        >
+                          &#8249;
                       </button>
-                    </li>
-                  )}
+                      </li>
+                    )}
 
-                  {playerList.length >= 10 ? (
-                    <li className="page-item">
-                      <button
-                        className="page-link"
-                        aria-label="Next"
-                        onClick={() => setPageInitial(pageInitial + 1)}
-                      >
-                        &#8250;
+                    {playerList.length >= 10 ? (
+                      <li className="page-item">
+                        <button
+                          className="page-link"
+                          aria-label="Next"
+                          onClick={() => setPageInitial(pageInitial + 1)}
+                        >
+                          &#8250;
                       </button>
-                    </li>
-                  ) : (
-                    <li className="page-item">
-                      <button
-                        className="page-link disabled"
-                        disabled
-                        onClick={() => setPageInitial(pageInitial + 1)}
-                      >
-                        &#8250;
+                      </li>
+                    ) : (
+                      <li className="page-item">
+                        <button
+                          className="page-link disabled"
+                          disabled
+                          onClick={() => setPageInitial(pageInitial + 1)}
+                        >
+                          &#8250;
                       </button>
-                    </li>
-                  )}
-                </ul>
+                      </li>
+                    )}
+                  </ul>
+                </div>
               </div>
             </div>
           </div>
